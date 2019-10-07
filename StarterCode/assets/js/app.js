1b5be39af1dcb1a0fe5818366fd1c4ad79cf42be
Elizabeth Duenas <salgadoe88@gmail.com>
2019-10-06 21:47:47 -0500

    updated
    update

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -24,6 +24,14 @@ var svg = d3
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+d3.csv("assets/data.csv").then(function(data){
+  console.log(data); 
+
+  data.forEach(function(data){
+    data.smokes = +data.smokes;
+    data.age = +data.age;
+  }); 
+});   
 // Initial Params
 var chosenXAxis = "poverty";
 
